51507f848f73cba425dbb97030132dba0d50549c
Dhruval Patel <dhruvalpatel556@gmail.com>
2021-04-29 21:30:22 +0530

    Add Code

---
+++ services/src/main/java/muni/pa165/services/CourtService.java
@@ -0,0 +1,28 @@
+package muni.pa165.services;
+
+import muni.pa165.persistence.entity.Court;
+
+import java.util.List;
+import java.util.Optional;
+public interface CourtService {
+
+    void createCourt(Court c);
+
+    /**
+     * Get all registered Court
+     */
+    List<Court> getAllCourt();
+
+
+    Optional<Court> findById(Long id);
+
+    /**
+     * remove Court
+     */
+    void remove(Court court);
+
+
+    List<Court> findByName(String name);
+
+
+}
---
+++ services/src/main/java/muni/pa165/services/CourtServiceImpl.java
@@ -0,0 +1,35 @@
+package muni.pa165.services;
+
+import muni.pa165.persistence.dao.CourtDao;
+import muni.pa165.persistence.entity.Court;
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Optional;
+
+public class CourtServiceImpl implements CourtService{
+
+    @Inject
+    private CourtDao courtDao;
+    private String name;
+
+
+    @Override
+    public void createCourt(Court c) {
+        courtDao.create(c);
+    }
+
+    @Override
+    public List<Court> getAllCourt() {return courtDao.findAll();}
+
+    @Override
+    public Optional<Court> findById(Long id) { return courtDao.findById(id); }
+
+    @Override
+    public void remove(Court court) { courtDao.remove(court); }
+
+    @Override
+    public List<Court> findByName(String name) {
+        this.name = name;
+        return courtDao.findByName(name);
+    }
+}
